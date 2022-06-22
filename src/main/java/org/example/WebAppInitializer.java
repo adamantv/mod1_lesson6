b80ca0dba5ffdc4c56829521f962335edecd2345
valeria.golovko <valeria.golovko@megafon.ru>
2022-06-22 16:30:17 +0400

    used Java-based config Spring MVC

---
+++ src/main/java/org/example/WebAppInitializer.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import org.apache.log4j.Logger;
+import org.example.web.config.WebContextConfig;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import java.util.EnumSet;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+    private final Logger logger = Logger.getLogger(WebAppInitializer.class);
+
+    @Override
+    public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
+        logger.info("loading app config");
+
+        XmlWebApplicationContext applicationContext = new XmlWebApplicationContext();
+        applicationContext.setConfigLocation("classpath:app-config.xml");
+        servletContext.addListener(new ContextLoaderListener(applicationContext));
+
+        logger.info("loading web config");
+//        XmlWebApplicationContext webApplicationContext = new XmlWebApplicationContext();
+//        webApplicationContext.setConfigLocation("classpath:web-config.xml");
+        AnnotationConfigWebApplicationContext webApplicationContext = new AnnotationConfigWebApplicationContext();
+        webApplicationContext.register(WebContextConfig.class);
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(webApplicationContext);
+
+        logger.info("loading dispatcher");
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", dispatcherServlet);
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+
+        EnumSet<DispatcherType> dispatcherTypes = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD);
+
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+
+        FilterRegistration.Dynamic characterEncoding = servletContext.addFilter("characterEncoding", characterEncodingFilter);
+        characterEncoding.addMappingForUrlPatterns(dispatcherTypes, true, "/*");
+    }
+}
